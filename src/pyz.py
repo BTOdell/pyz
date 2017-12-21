a62ee47434315d66b766b889dfc61b46f90f19f1
Bradley Odell <btodell@hotmail.com>
2017-12-20 16:44:00 -0800

    Improved configurability of build operation.

--- src/pyz.py
+++ src/pyz.py
@@ -1,3 +1,7 @@
+"""
+Provides utility functions to build a Python zip application.
+"""
+
 import fnmatch
 import os
 import shutil
@@ -22,55 +26,87 @@ if {0}:
 """.lstrip()
 
 
-def build(out_file_path, working_directory, main_path, includes,
-          unixify=None,
+def build(out_file_path, main_module_path, includes,
+          include_directory=None,
           main_function=None,
-          version_requirement=None):
+          version_requirement=None,
+          archive_source_base=None,
+          unixify=None):
     """
     Builds a "standalone" Python application by bundling the source code
     and any virtual environment dependencies into an executable zip archive.
 
-    :param out_file_path:
+    :param out_file_path: The output file path of the Python zip application.
     :type out_file_path: str
-    :param working_directory:
-    :type working_directory: str
-    :param main_path:
-    :type main_path: str
-    :param includes:
+    :param main_module_path: The path to the main module (relative to the include directory, without the .py extension).
+    :type main_module_path: str
+    :param includes: A list of include items to be added to the Python zip application.
     :type includes: list[Include]
-    :param unixify:
-    :type unixify: str|None
+    :param include_directory: The base directory to add include items from.
+    :type include_directory: str|None
     :param main_function: The name of the main function to invoke.
     :type main_function: str|None
     :param version_requirement: The range of versions to check against the running interpreter version.
     :type version_requirement: VersionRequirement|None
+    :param archive_source_base: The base directory in the zip application to include all source files.
+    :type archive_source_base: str|None
+    :param unixify: Additional information to output a unix-like application.
+    :type unixify: Unixify|None
     :return: None
     """
-    archive_base = "app"
-    old_cwd = os.getcwd()
-    os.chdir(working_directory)
-    try:
-        # Create zip file
-        zip_file_path = os.path.join(old_cwd, out_file_path + ".pyz")
-        with zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED, False) as zip_app:
-            # Add files to zip file
-            for include in includes:
-                __add_include_path(zip_app, archive_base, include)
+    archive_source_base_path = "dist" if archive_source_base is None else archive_source_base
+
+    def write_zip_app(file_path, mode='w'):
+        # Note: Must use try-finally instead of 'with' context manager in order to maintain 2.6 compatibility.
+        pyz = zipfile.ZipFile(file_path, mode, zipfile.ZIP_DEFLATED, False)
+        try:
+            def add_includes():
+                # Add files to zip file
+                for include in includes:
+                    __add_include_path(pyz, archive_source_base_path, include)
+
+            if include_directory is None:
+                add_includes()
+            else:
+                # Change to include directory
+                old_cwd = os.getcwd()
+                os.chdir(include_directory)
+                try:
+                    add_includes()
+                finally:
+                    os.chdir(old_cwd)
+
             # Generate __main__.py for zip file
-            zip_app.writestr("__main__.py", __generate_main(os.path.join(archive_base, main_path),
-                                                            main_function=main_function,
-                                                            version_requirement=version_requirement))
-        # If 'unixify' is truthy, then make an executable version without an extension
-        if unixify:
-            unix_out_file_path = os.path.join(old_cwd, out_file_path)
-            with open(unix_out_file_path, "wb") as unix_app:
-                # Write shabang
-                unix_app.write("#!/usr/bin/env {0}\n".format(unixify).encode())
-                # Append the zip application
-                with open(zip_file_path, "rb") as zip_app:
-                    shutil.copyfileobj(zip_app, unix_app)
-    finally:
-        os.chdir(old_cwd)
+            pyz.writestr("__main__.py",
+                         __generate_main(os.path.join(archive_source_base_path, main_module_path),
+                                         main_function=main_function,
+                                         version_requirement=version_requirement))
+        finally:
+            pyz.close()
+
+    if unixify is None:
+        # Write one zip application file
+        write_zip_app(out_file_path)
+    else:
+        def write_unix(file_path, append_file_path=None):
+            with open(file_path, "wb") as unix_app:
+                # Write shebang
+                unix_app.write("#!{0}\n".format(unixify.shebang).encode())
+                # Optionally append another file
+                if append_file_path is not None:
+                    with open(append_file_path, "rb") as append_file:
+                        shutil.copyfileobj(append_file, unix_app)
+
+        unix_out_file_path = unixify.out_file_path
+        if unix_out_file_path is None:
+            # Write one unix zip application file
+            write_unix(out_file_path)
+            write_zip_app(out_file_path, mode='a')
+        else:
+            # Write non-unix zip application file
+            write_zip_app(out_file_path)
+            # Write unix zip application file
+            write_unix(unix_out_file_path, append_file_path=out_file_path)
 
 
 def __generate_main(main_path, main_function=None, version_requirement=None):
@@ -81,7 +117,8 @@ def __generate_main(main_path, main_function=None, version_requirement=None):
     :type main_path: str
     :param main_function: The name of the main function to invoke.
     :type main_function: str|None
-    :param version_requirement: The range of versions to check against the running interpreter version.
+    :param version_requirement: The range of versions to check
+                                against the running interpreter version.
     :type version_requirement: VersionRequirement|None
     :return: The generated __main__.py file content.
     :rtype: str
@@ -102,9 +139,9 @@ def __generate_main(main_path, main_function=None, version_requirement=None):
         else:
             maximum_version_str = " {0} {1}".format("<" if version_requirement.exclusive_maximum else "<=",
                                                     ".".join(map(str, version_requirement.maximum)))
-            maximum_version_check_template = "(v >= {0})" if version_requirement.exclusive_maximum else "(v > {0})"
-            version_checks.append(maximum_version_check_template.format(str(version_requirement.maximum)))
-        if len(version_checks) > 0:
+            maximum_version_check = "(v >= {0})" if version_requirement.exclusive_maximum else "(v > {0})"
+            version_checks.append(maximum_version_check.format(str(version_requirement.maximum)))
+        if version_checks:
             version_check = " or ".join(version_checks)
             version_code = __VERSION_CHECK_TEMPLATE.format(version_check, minimum_version_str, maximum_version_str)
     # Generate __main__.py file content
@@ -118,11 +155,11 @@ def __add_include_path(zip_file, archive_path, include):
     """
     Adds the given include item to the zip archive.
 
-    :param zip_file:
+    :param zip_file: The zip file to add the include item to.
     :type zip_file: zipfile.ZipFile
-    :param archive_path:
+    :param archive_path: The base path in the zip file to add the include item relative to.
     :type archive_path: str
-    :param include:
+    :param include: Specifies resource(s) to include in the zip file.
     :type include: Include
     """
     if os.path.isfile(include.source_path):
@@ -181,3 +218,26 @@ class VersionRequirement(object):
         self.minimum = minimum
         self.maximum = maximum
         self.exclusive_maximum = exclusive_maximum
+
+
+class Unixify(object):
+    """
+    Provides information to output a unix-like executable.
+    """
+
+    def __init__(self, out_file_path=None, shebang=None):
+        """
+        Creates a specification for building a unix-like executable.
+
+        :param out_file_path: The output file path of a second executable.
+                              If None, then the main output file path is used
+                              for the unix-like executable and only one file is generated.
+        :type out_file_path: str|None
+        :param shebang: The shebang string to insert at the start of the zip file to enable unix-like behavior.
+                        This string should not include the shebang (#!) itself or a newline at the end.
+                        Default: /usr/bin/env python
+        :type shebang: str|None
+        """
+        super(Unixify, self).__init__()
+        self.out_file_path = out_file_path
+        self.shebang = "/usr/bin/env python" if shebang is None else shebang
